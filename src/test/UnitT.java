49a468bad99532863208549549f2fa784ea9f28f
Rohan Kalkumbe <rohankalkumbe31@gmail.com>
2024-11-05 11:25:33 +0530

    first commit

---
+++ src/test/UnitT.java
@@ -0,0 +1,11 @@
+package com.keenconcept.devTest;
+
+import org.testng.annotations.Test;
+
+public class UnitT {
+
+    @Test
+    public void unitTest() {
+        System.out.println("Unit Test is executed");
+    }
+}
